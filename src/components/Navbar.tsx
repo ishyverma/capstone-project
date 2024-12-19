edeb4110ff3640a801dc5d4ef553dbaed8fee312
ishyverma <ishyamver@gmail.com>
2024-12-19 22:22:30 +0530

    added boldness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 export const Navbar = () => {
     return <div className="flex justify-between font-domine py-4 text-[#23281B]">
         <div>
-            <span className="text-2xl">EcoPortfolio</span>
+            <span className="text-2xl font-bold">EcoPortfolio</span>
         </div>
         <div className="gap-8 text-lg items-center hidden sm:flex">
             <span className="cursor-pointer">Home</span>
